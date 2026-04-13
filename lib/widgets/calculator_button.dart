d6ad78c6dd4f3e450c321ade80d002eb5c5a7fb2
Hien Dao <hienqdao5@gmail.com>
2026-04-13 13:09:24 -0400

    Add calculator button widget

---
+++ lib/widgets/calculator_button.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class CalculatorButton extends StatelessWidget {
+  final String text;
+  final VoidCallback onPressed;
+
+  const CalculatorButton({
+    super.key,
+    required this.text,
+    required this.onPressed,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+      onPressed: onPressed,
+      child: Text(text),
+      style: ElevatedButton.styleFrom(
+        padding: const EdgeInsets.all(20),
+        textStyle: const TextStyle(fontSize: 24),
+      ),
+    );
+  }
+}
