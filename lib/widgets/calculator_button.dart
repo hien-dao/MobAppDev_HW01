95e866bb6e44c4a10758492fd27a4d6f0e651461
Hien Dao <hienqdao5@gmail.com>
2026-04-13 13:56:34 -0400

    Update calculator button color

--- lib/widgets/calculator_button.dart
+++ lib/widgets/calculator_button.dart
@@ -3,24 +3,33 @@ import 'package:flutter/material.dart';
 class CalculatorButton extends StatelessWidget {
   final String text;
   final VoidCallback onPressed;
+  final Color? backgroundColor;
 
   const CalculatorButton({
     super.key,
     required this.text,
     required this.onPressed,
+    this.backgroundColor,
   });
 
   @override
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      child: Text(text),
       style: ElevatedButton.styleFrom(
+        backgroundColor: backgroundColor,
         padding: const EdgeInsets.all(15),
         textStyle: const TextStyle(fontSize: 24),
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(12),
         ),
+      ),
+      child: Text(
+        text,
+        style: TextStyle(
+          color: Colors.white,
+          fontWeight: FontWeight.bold,
+          ),
       ),
     );
   }
